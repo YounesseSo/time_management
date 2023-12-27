8d007e5a18df470978d4c6c3cd670da53e1f29cb
FayssalG <fayssaldimawac123@gmail.com>
2023-12-27 01:39:45 +0100

    Initial commit

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,40 @@
+import fallingClockPic from '../../assets/clock-with-bright-falling-sequins.jpg'
+import styles from './Home.module.css'
+
+export default function Home() {
+  return (
+    <>
+      <div className={styles.hero_container}>
+          <h1 className={styles.title}>La gestion de temps</h1>
+
+          <div className={styles.details}>
+              <ul>
+                <span>Réalisé par</span>
+                <li>Snipe harvard</li>
+                <li>Snipe harvard</li>
+                <li>Snipe harvard</li>
+              </ul>
+
+              <ul>
+                <span>Encadrée par</span> 
+                <li>Teacher</li>
+              </ul>
+          </div>
+
+      </div>
+
+      <div className={styles.plan_container}>
+          <div className={styles.plan}>
+            <button className={styles.btn} role="button">Introduction</button>
+            <button className={styles.btn} role="button">L'importance de gestion du temps</button>
+            <button className={styles.btn} role="button">Les défis de la gestion du temps</button>
+            <button className={styles.btn} role="button">Les methodes de gestion du temps</button>
+            <button className={styles.btn} role="button">Conclusion</button>
+
+          </div>
+
+      </div>
+    </>
+    
+  )
+}
